c9927fa609b61b9f569b4ca0668161fd2389f612
Gary Woodfine <gary.woodfine@threenine.co.uk>
2020-09-13 20:44:56 +0100

    setting up the project

---
+++ README.md
@@ -0,0 +1,10 @@
+
+#
+
+
+
+
+
+ ### Developed, Sponsored and Supported 
+ 
+[![threenine logo](http://static.threenine.co.uk/img/github_footer.png)](https://threenine.co.uk/)
---
+++ gridsome.client.js
@@ -0,0 +1,22 @@
+
+export default function (Vue, options, context) {
+
+    if(!options.affiliateId){
+        console.error('affiliate Id not set')
+    }
+
+    if(!options.affiliateUrl) options.affiliateUrl = '//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId='
+
+    Vue.prototype.$oneLink = function () {}
+
+    function setupOneLink(options){
+
+    }
+}
+
+
+/*
+
+<div id="amzn-assoc-ad-2234234234-2342342143-213412341232-2134123"></div>
+<script async src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=2234234234-2342342143-213412341232-2134123"></script>
+ */
