148ecd62b4fed484ed83028879f9371d7fa0eb95
Gary Woodfine <gary.woodfine@threenine.co.uk>
2020-09-14 08:50:32 +0100

    Updated Linting

--- gridsome.client.js
+++ gridsome.client.js
@@ -2,13 +2,12 @@ export default function (Vue, options, {isClient}) {
     if(!isClient)return
 
     if (!options.id || options.id ==='') {
-        console.error("Amazon Onelink affiliate Id not set");
+        console.error("Amazon Onelink affiliate Id not set")
     }
 
     function setUpScript(options){
         let scriptLink = `//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=${options.id}`
-
-       return `<script async src="${scriptLink}"></script>`
+        return `<script async src="${scriptLink}"></script>`
     }
     function setupDiv(options){
         let divId = `amzn-assoc-ad-${options.id}`
